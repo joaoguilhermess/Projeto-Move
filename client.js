f393fba171ae4aed97e50437bf3e29db7af2252c
Jotagê <71993919+JG-entusiasta@users.noreply.github.com>
2022-08-20 16:51:51 -0300

    Add files via upload

--- client.js
+++ client.js
@@ -5,27 +5,89 @@ const stream = require("stream");
 const Port = 6000;
 const Host = "192.168.0.107";
 
-var client = new net.Socket();
+function isDir(name) {
+	return fs.statSync(name).isDirectory();
+}
 
-client.connect(Port, Host);
+class Client {
+	static setHost(host, port) {
+		this.port = port;
+		this.host = host;
+	}
 
-client.on("connect", function() {
-	var args = process.argv.slice(2);
-	var name = args[0];
+	static connect() {
+		var socket = new net.Socket();
 
-	console.log("Sending File:", name);
+		socket.connect(this.port, this.host);
 
-	client.write(name.replaceAll(" ", ">"));
+		return new Promise(function(resolve, reject) {
+			socket.on("connect", function() {
+				resolve(socket);
+			});
+		});
+	}
 
-	var fileStream = fs.createReadStream(name);	
+	static async IterateDir(path) {
+		var list = fs.readdirSync(path);
 
-	stream.pipeline(fileStream, client, function(e) {
-		if (!e) {
-			console.log("File Sent:", name);
-			client.end();
-			fileStream.close();
-		} else {
-			console.log("error", e);
+		for (var i = 0; i < list.length; i++) {
+			var n = path + "/" + list[i];
+
+			if (isDir(n)) {
+				await this.SendDirectory(n);
+				this.IterateDir(n);
+			} else {
+				await this.SendFile(n);
+			}
 		}
-	});
-});
+	}
+
+	static async SendFile(name) {
+		var socket = await this.connect();
+
+		return new Promise(function(resolve, reject) {
+			console.log("Sending File:", name);
+
+			socket.write("file>" + name.replaceAll(" ", "?") + ">" + fs.statSync(name).size);
+
+			var fileStream = fs.createReadStream(name);	
+
+			stream.pipeline(fileStream, socket, function(e) {
+				if (!e) {
+					console.log("File Sent:", name);
+					socket.end();
+					fileStream.close();
+					resolve();
+				} else {
+					console.log("error", e);
+				}
+			});
+		});
+	}
+
+	static async SendDirectory(name) {
+		var socket = await this.connect();
+
+		return new Promise(function(resolve, reject) {
+			console.log("Sending Directory:", name);
+
+			socket.write("dir>" + name.replaceAll(" ", "?"));
+
+			socket.end();
+			resolve();
+		});
+	}
+}
+
+
+var name = (process.argv.slice(2))[0];
+
+Client.setHost(Host, Port);
+
+if (isDir(name)) {
+	Client.SendDirectory(name);
+	console.log("a");
+	Client.IterateDir(name);
+} else {
+	Client.SendFile(name);
+}
--- server.js
+++ server.js
@@ -11,23 +11,44 @@ var app = net.createServer(function (socket) {
 	}
 
 	socket.on("data", function(data) {
+
 		if (!socket.file) {
-			var args = data.toString().split(" ");
-			socket.file = args[0].replaceAll(">", " ");
+			var args = data.toString().split(">");
 			
-			console.log("Receiving File:", socket.file);
+			if (args[0] == "dir") {
+				if (!fs.existsSync(FilesPath + args[1].replaceAll("?", " "))) {
+					fs.mkdirSync(FilesPath + args[1].replaceAll("?", " "));
+				}
 
-			socket.fileStream = fs.createWriteStream(FilesPath + socket.file);
+				socket.type = "dir";
+				socket.dir = args[1];
+			} else { 
+				socket.type = "file";
+				socket.file = args[1].replaceAll("?", " ");
+				socket.fileSize = parseInt(args[2]);
+				socket.current = 0;
 
-			socket.fileStream.write(args.slice(1).join(" "));
-		} else {
+				socket.fileStream = fs.createWriteStream(FilesPath + socket.file);
+
+				socket.fileStream.write(args.slice(3).join(" "));
+			}
+
+		} else if (socket.type == "file") {
 			socket.fileStream.write(data);
+
+			socket.current += data.length;
+		
+			process.stdout.write("\r\x1b[1m\x1b[30m" + "Receiving File: " + socket.file + " " + (socket.current/socket.fileSize * 100).toFixed(1) + "%" + "\x1b[0m");
 		}
 	});
 
 	socket.on("end", function() {
-		console.log("File Received:", socket.file);
-		socket.fileStream.close();
+		if (socket.type == "dir") {
+			console.log("\nDirecory Received:", socket.dir);
+		} else {
+			console.log("\nFile Received:", socket.file);
+			socket.fileStream.close();
+		}
 	});
 });
 
